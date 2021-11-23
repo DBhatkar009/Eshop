eefd6669c855c01922c497e66578b9465c3ba986
dhananjay bhatkar <dhananjaybhatkar@idcletech.com>
2021-11-24 00:16:38 +0530

    Adding new components and styling.

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'redbits-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'admin-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'admin';
+  title = 'admin';
 }
