1f7c349508c18f34ccd518d6c9ff122b326c2d91
dhananjay bhatkar <dhananjaybhatkar@idcletech.com>
2021-12-17 17:27:54 +0530

    Sidebar styling

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -10,81 +10,20 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BasicFormComponent } from './pages/basic-form/basic-form.component';
 
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule } from '@angular/material/core';
-import {MatTabsModule } from '@angular/material/tabs';
-import {MatIconModule} from '@angular/material/icon';
-
-import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
-import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbValidationModule} from 'mdb-angular-ui-kit/validation';
-
-
-
-
-
-
-
-
-const routes: Routes = [{path: '', component: HomePageComponent},
-{path:'products', component: ProductListComponent},{path: 'basicform', component: BasicFormComponent}]
+const routes: Routes = [{ path: '', component: HomePageComponent },
+{ path: 'products', component: ProductListComponent }];
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent, BasicFormComponent],
+  declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
-     RouterModule.forRoot(routes),
-     UiModule,
-      BrowserAnimationsModule,
-      ReactiveFormsModule,
-      MatInputModule,
-      MatSelectModule,
-      MatButtonModule,
-      MatCheckboxModule,
-      MatChipsModule,
-      MatDatepickerModule,
-      MatTabsModule,
-      MatNativeDateModule,
-      MatIconModule,
-      MdbAccordionModule,
-      MdbCarouselModule,
-      MdbCheckboxModule,
-      MdbCollapseModule,
-      MdbDropdownModule,
-      MdbFormsModule,
-      MdbModalModule,
-      MdbPopoverModule,
-      MdbRadioModule,
-      MdbRangeModule,
-      MdbRippleModule,
-      MdbScrollspyModule,
-      MdbTabsModule,
-      MdbTooltipModule,
-      MdbValidationModule,
-      MdbTabsModule,
-     
-
-    ],
+    RouterModule.forRoot(routes),
+    UiModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
